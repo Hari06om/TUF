0beb048e00a228102a197f0b7d5c1e5cdc09b23e
Hari Om Verma <hariohmverma96@gmail.com>
2026-05-04 20:06:28 +0530

    Remove unnecessary blank lines in Pattern8.java

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -5,7 +5,6 @@ public class Pattern8 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("_");
@@ -13,10 +12,8 @@ public class Pattern8 {
             for (int j = 0; j < 2 * (n - i) - 1; j++) {
                 System.out.print("*");
             }
-
             System.out.println();
         }
-
         sc.close();
     }
 }
