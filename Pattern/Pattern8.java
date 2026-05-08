92a1b6a093e280873516c98fb0d10ab964cf09ca
Hari Om Verma <hariohmverma96@gmail.com>
2026-05-08 11:17:24 +0530

    add Pattern9.java and its compiled class file; modify Pattern8.java to comment out the code that prints asterisks and replace it with an empty loop, then recompile it.

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -1,5 +1,24 @@
 import java.util.Scanner;
 
+// public class Pattern8 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number of rows: ");
+//         int n = sc.nextInt();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 System.out.print("_");
+//             }
+//             for (int j = 0; j < 2 * (n - i) - 1; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
 public class Pattern8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,12 +27,10 @@ public class Pattern8 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("_");
-            }
-            for (int j = 0; j < 2 * (n - i) - 1; j++) {
-                System.out.print("*");
+            
             }
             System.out.println();
         }
         sc.close();
     }
-}
+}   
