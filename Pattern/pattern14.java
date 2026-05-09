42bd495f4c91a8076c55d55cc905c5fae0c9fc26
Hari Om Verma <hariohmverma96@gmail.com>
2026-05-09 10:44:53 +0530

    pattern13: Add new pattern program to print numbers in a triangular format pattern14: Add new pattern program to print characters in a triangular format

--- Pattern/pattern14.java
+++ Pattern/pattern14.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class pattern13{
+
+public class pattern14 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n =sc.nextInt();
-        int a = 1;
+        char a = 'A';
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j =1;j<=i;j++){
                 System.out.print(a+" ");
                 a++;
             }
