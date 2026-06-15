6404fb870bec3ede14751c4a3e61e0d625dcf118
Hari Om Verma <hariohmverma96@gmail.com>
2026-06-15 21:17:48 +0530

    Refactor practice.java to remove unused method and implement pattern printing

--- Recursion/practice.java
+++ Recursion/practice.java
@@ -1,10 +1,21 @@
 public class practice {
 
-    public static void f(){
-        System.err.println("Hello World");
-        f();
-    }
     public static void main(String[] args) {
-        f();
+        int n = 5;
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        if (n == 0) {
+            return;
+        }
+        printPattern(n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
     }
 }
+
+
+
