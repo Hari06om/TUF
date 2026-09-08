84cba924ca1d1d6dacf00b764734c8377c4575c0
Hari Om Verma <hariohmverma96@gmail.com>
2026-09-08 15:55:04 +0530

    add the revison file

---
+++ DSA_Tuf/Basic/count.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class count {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt =0;
+
+        while(n>0){
+            n = n/10;
+            cnt = cnt + 1;
+        }
+        System.out.println(cnt);
+
+    }
+}
